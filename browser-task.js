0f99bc738e834a37d571cd78253b630913cd85cd
Erik Pukinskis <erik@snowedin.net>
2017-02-27 16:50:19 -0800

    error message

--- browser-task.js
+++ browser-task.js
@@ -212,8 +212,19 @@ module.exports = library.export(
     }      
 
     Browser.prototype.assertText =
-      function(selector, pattern, callback) {
+      function(selector, pattern, callback1, callback2) {
 
+        if (typeof selector != "string") {
+          throw new Error("First argument to browser.assertText needs to be a selector for the element to search. You passed ", selector)
+        }
+
+        var isString = typeof pattern != "string"
+        var isRegExp = !isString && pattern.constructor && pattern.constructor.name == "RegExp"
+
+        if (!isString && !isRegExp) {
+          throw new Error("Second argument to browser.assertText needs to be a string or a regular expression to search for. You passed "+pattern)
+        }
+        
         assertMinion(this, "assertText")
         assertRetained(this)
 
