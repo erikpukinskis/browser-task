fd145b972291b4d24d9235c4a623804682391c52
Erik Pukinskis <erik@snowedin.net>
2018-01-15 14:06:58 -0800

    open the dang window for me

--- start.js
+++ start.js
@@ -1,1 +1,4 @@
-require("module-library")(require).using(["./start-server"], function(start) { start(9777) })
+require("module-library")(require).using(["./start-server", "child_process"], function(start, childProcess) {
+  start(9777)
+  childProcess.exec("open http://localhost:9777/minions")
+})
